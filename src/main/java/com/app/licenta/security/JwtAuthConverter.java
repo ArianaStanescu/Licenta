dbb67c6de908f23fc428c24f5f3e4785bfcb71fc
Ariana Stanescu <stanescu.ariana02@gmail.com>
2025-02-08 15:43:43 +0200

    first structure of authorization

---
+++ src/main/java/com/app/licenta/security/JwtAuthConverter.java
@@ -0,0 +1,48 @@
+package com.app.licenta.security;
+
+import com.nimbusds.jose.shaded.gson.internal.LinkedTreeMap;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+//@ConditionalOnProperty(name = "authorization.enabled", havingValue = "true")
+public class JwtAuthConverter implements Converter<Jwt, AbstractAuthenticationToken> {
+
+    private final JwtGrantedAuthoritiesConverter jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+
+    @Override
+    public AbstractAuthenticationToken convert(Jwt jwt) {
+        Collection<GrantedAuthority> authorities = Stream
+                .concat(jwtGrantedAuthoritiesConverter.convert(jwt).stream(), extractResourceRoles(jwt).stream())
+                .collect(Collectors.toSet());
+        return new JwtAuthenticationToken(jwt, authorities, getPrincipalClaimName(jwt));
+    }
+
+    private String getPrincipalClaimName(Jwt jwt) {
+        return jwt.getClaim("email");
+    }
+
+    @SuppressWarnings("unchecked")
+    private Collection<? extends GrantedAuthority> extractResourceRoles(Jwt jwt) {
+        final var resourceAccess = (Map<String, LinkedTreeMap<?, ?>>) jwt.getClaims().getOrDefault("resource_access", Map.of());
+
+        final var resourceAccessLicentaBackendClient = (LinkedTreeMap<String, List<String>>) resourceAccess.getOrDefault("licenta-backend-client", new LinkedTreeMap<String, List<String>>());
+        final var licentaBackendClientRoles = resourceAccessLicentaBackendClient.getOrDefault("roles", List.of());
+
+        return licentaBackendClientRoles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toList());
+    }
+}
---
+++ src/main/java/com/app/licenta/security/WebSecurityConfig.java
@@ -0,0 +1,58 @@
+package com.app.licenta.security;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.firewall.DefaultHttpFirewall;
+import org.springframework.security.web.firewall.HttpFirewall;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+//@ConditionalOnProperty(name = "authorization.enabled", havingValue = "true")
+public class WebSecurityConfig {
+
+    private static final String TRAINER_ROLE = "trainer";
+    private static final String PARENT_ROLE = "parent";
+    private final JwtAuthConverter jwtAuthConverter;
+
+    public WebSecurityConfig(JwtAuthConverter jwtAuthConverter) {
+        super();
+        this.jwtAuthConverter = jwtAuthConverter;
+    }
+
+    @Bean
+    public HttpFirewall getHttpFirewall() {
+        return new DefaultHttpFirewall();
+
+//		StrictHttpFirewall strictHttpFirewall = new StrictHttpFirewall();
+//		strictHttpFirewall.setAllowSemicolon(true);
+//		return strictHttpFirewall;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .cors().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/trainers/register", "/parents/register", "/error")
+                .permitAll()
+                .requestMatchers("/**")
+                .hasAnyRole(TRAINER_ROLE, PARENT_ROLE)
+                .anyRequest()
+                .authenticated();
+
+        http.oauth2ResourceServer().jwt().jwtAuthenticationConverter(jwtAuthConverter);
+
+        return http.build();
+    }
+
+}
